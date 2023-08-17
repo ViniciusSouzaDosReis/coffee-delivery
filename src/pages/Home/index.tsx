e452b3103e9f9eca5a33d1048cb3d0d06b052e45
Vinicius Reis <vinireis010305@gmail.com>
2023-08-17 11:38:49 -0300

    Criando pagina Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Header";
+import { SectionIntro } from "./components/SectionIntro";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <SectionIntro />
+    </>
+  )
+}
+
+export { Home }
