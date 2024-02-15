4b67de444c68d40ac28014c772f2a13a0ff6a4c4
Vinicius Reis <vinireis010305@gmail.com>
2024-02-15 16:50:49 -0300

    add navLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,25 +5,30 @@ import logoCoffee from "../../assets/Logo.svg";
 import { HeaderContainer, CartContainer } from "./styles";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
+import { NavLink } from "react-router-dom";
 
 function Header() {
   const { products } = useContext(CartContext);
 
   return (
     <HeaderContainer>
-      <span>
-        <img src={logoCoffee} alt="" />
-      </span>
+      <NavLink to="/" title="Home">
+        <span>
+          <img src={logoCoffee} alt="" />
+        </span>
+      </NavLink>
       <div>
         <div>
           <MapPin size={24} color="#8047F8" weight="fill" />
           Porto Alegre, RS
         </div>
         <CartContainer>
-          <button>
-            <ShoppingCart size={24} color="#C47F17" weight="fill" />
-          </button>
-          {products.length > 0 ? <span>{products.length}</span> : ""}
+          <NavLink to="checkout" title="Checkout">
+            <button>
+              <ShoppingCart size={24} color="#C47F17" weight="fill" />
+            </button>
+            {products.length > 0 ? <span>{products.length}</span> : ""}
+          </NavLink>
         </CartContainer>
       </div>
     </HeaderContainer>
