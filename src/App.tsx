da42e56b0cbf340eee00dd8e74be9ff629fa1a8f
Vinicius Reis <vinireis010305@gmail.com>
2024-02-15 16:24:21 -0300

    add CardContextProvider arount Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 
 import { Home } from "./pages/Home";
+import { CartContextProvider } from "./contexts/CartContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Home />
+      <CartContextProvider>
+        <Home />
+      </CartContextProvider>
       <GlobalStyle />
     </ThemeProvider>
-  )
+  );
 }
