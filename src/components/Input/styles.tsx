46f15030068a633c121e5ae34e7387bfb3facced
Vinicius Reis <vinireis010305@gmail.com>
2024-02-19 11:14:36 -0300

    change width of input

--- src/components/Input/styles.tsx
+++ src/components/Input/styles.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const InputContainer = styled.div`
   position: relative;
-  max-width: 35rem;
+  min-width: 3.75rem;
 `;
 
 export const InputContent = styled.input`
